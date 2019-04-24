5749602f593f72fce259ef70e54e9456729aeb3d
Monteth <greezlie@gmail.com>
2019-04-24 13:00:11 +0200

    Hotfix

--- src/resolvers.js
+++ src/resolvers.js
@@ -3,7 +3,6 @@ import {withFilter} from 'graphql-subscriptions';
 import Conversation from './models/conversation'
 import Message from './models/message'
 import User from './models/user'
-import Advert from './models/advert'
 
 const pubsub = new PubSub();
 
@@ -11,23 +10,11 @@ export const resolvers = {
     Query: {
         conversation: (root, {id}) => {
             return Conversation.findById(id);
-            // return conversations.find(conversation => conversation.id === id)
         },
         conversations: () => {
             return Conversation.find({})
         },
         searchConversation: (root, {search}) => {
-            // console.log(search);
-            // return Conversation.find({})
-            // return Conversation
-            //     .find( { text: { $search : search.name } },
-            //         { score : { $meta: "textScore" } } )
-            // return Conversation.find({name: new RegExp('^'+search.name+'$', "i")});
-            // return Conversation.find({name: {$in: search.name}});
-            // return Conversation.find({name: {$in: search.name}, tags: {$in: search.tags}})
-            // {$or:[{region: "NA"},{sector:"Some Sector"}]}
-            // return User.find({_id: {$in: parent.contributorsIds}})
-
             return Conversation.find(
                 {
                     $and: [
@@ -36,17 +23,7 @@ export const resolvers = {
                     ]
                 },
             );
-
-            // {
-            //     $and: [
-            //         {$or: [{a: 1}, {b: 1}]},
-            //         {$or: [{c: 1}, {d: 1}]}
-            //     ]
-            // }
-
-            // PersonModel.find({ favouriteFoods: { "$in" : ["sushi"]} }, ...);
         }
-
     },
     Mutation: {
         addConversation: (root, {conversation}) => {
@@ -73,18 +50,6 @@ export const resolvers = {
             console.log("Message received");
             return mssg.save();
         },
-
-        // addAdvert: (root, {advert}) => {
-        //     const newAdv = new Advert({
-        //         title: advert.title,
-        //         description: advert.description,
-        //         tags: advert.tags,
-        //         date: new Date().toISOString(),
-        //         id_sender: advert.id_sender
-        //     });
-        //     console.log(`Advert added ${advert.title}`);
-        //     return newAdv.save()
-        // }
     },
     Subscription: {
         messageAdded: {
